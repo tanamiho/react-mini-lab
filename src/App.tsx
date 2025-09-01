9170e50cda88e0a9d15dd2c1f543ff1595a09deb
たなみほ <tanakashamiko@gmail.com>
2025-09-01 20:13:24 +0900

    day1 リンク追加

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div style={{ padding: 16 }}>
       <nav style={{ display: "flex", gap: 12, marginBottom: 16 }}>
-
+        <NavLink to="/day01">Day1</NavLink>
       </nav>
       <Routes>
         <Route path="/" element={<div>Home（Dayを選んでね）</div>} />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,4 +3,5 @@ import Day00 from "./Day00";
 export const routes = [
     { path: "/", element: <Day00 /> },
     { path: "/day/00", element: <Day00 /> },
+    { path: "/day/01", element: <Day01 /> },
 ];
